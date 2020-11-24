bb0b03be5c51157322a9174825ee213d3d8d0c4a
ptabis <p.tabis@protonmail.com>
2020-11-24 22:27:34 +0100

    Init

---
+++ src/App.svelte
@@ -0,0 +1,44 @@
+<script>
+  import Quagga from "./svelte/Quagga.svelte";
+</script>
+
+<style>
+  :global(body) {
+    margin: 0;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+  .App {
+    text-align: center;
+  }
+  .App p {
+    margin: 0.4rem;
+  }
+
+  .App-header {
+    background-color: #f9f6f6;
+    color: #333;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+  }
+
+  @keyframes App-logo-spin {
+    from {
+      transform: scale(1);
+    }
+    to {
+      transform: scale(1.06);
+    }
+  }
+</style>
+
+<div class="App">
+  <header class="App-header">
+    <p>
+      <Quagga />
+    </p>
+  </header>
+</div>
