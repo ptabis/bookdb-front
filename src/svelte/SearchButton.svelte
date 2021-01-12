8ae54a606557a30c0d84af561bcea16bbce3eac7
ptabis <p.tabis@protonmail.com>
2021-01-12 22:39:11 +0100

    add SearchButton component

---
+++ src/svelte/SearchButton.svelte
@@ -0,0 +1,43 @@
+<script>
+    import { quaggaClosed } from "../stores/quaggaStore.js";
+    import searchIcon from "../../public/icons/search-solid.svg";
+    import timesIcon from "../../public/icons/times-solid.svg";
+
+    let quaggaState; // 0 - off/ 1 - on
+
+    quaggaClosed.subscribe(value => {
+        quaggaState = value 
+    });
+
+    function changeQuagga() {
+        (quaggaState == 0) ? quaggaClosed.set(1) : quaggaClosed.set(0);
+    }
+
+    function clickHandler() {
+        changeQuagga();
+    }
+</script>
+
+<style>
+    button[type="button"] {
+        border: none;
+        height: 70px;
+        width: 70px;
+        background-color: #A3DDF0;
+        color: black;
+        padding: 20px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        margin: 4px 2px;
+        border-radius: 50%;
+    }
+    img.search-icon {
+        color: black;
+        height: 30px;
+    }
+</style>
+
+<button type="button" on:click={() => {clickHandler()}}>
+    <img class="search-icon" src={quaggaState ? searchIcon : timesIcon} alt="search button">
+</button>
