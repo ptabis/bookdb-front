f1b9ecf001eddea2827272231057b3c6cb32d9c5
ptabis <p.tabis@protonmail.com>
2021-01-12 22:38:58 +0100

    refactor Quagga component

--- src/svelte/Quagga.svelte
+++ src/svelte/Quagga.svelte
@@ -1,114 +1,76 @@
 <script>
-  import { onMount, createEventDispatcher } from "svelte";
+  import { createEventDispatcher } from "svelte";
   import Quagga from "quagga";
+  import { quaggaClosed } from "../stores/quaggaStore.js";
 
   let dispatch = createEventDispatcher();
-  let status = "";
-  let error = "";
   let devices = [];
   let selectedDevice;
-  let closed = 0;
+  let closed = 1;
 
+  quaggaClosed.subscribe(value => {
+    if (closed != value) {
+      if (value == 0) init();
+      else close();
+    }
+  })
   async function _populateCameraDevices() {
     let videoDevices = await Quagga.CameraAccess.enumerateVideoDevices(),
       selectedDeviceLabel = Quagga.CameraAccess.getActiveStreamLabel();
     devices = videoDevices.map(({ id, deviceId, label }) => ({
       id: id || deviceId,
-      label: label || id || deviceId,
+      label: label || id || deviceId
     }));
     selectedDeviceLabel = devices.find(
-      (dev) => dev.label === selectedDeviceLabel
+      dev => dev.label === selectedDeviceLabel
     );
   }
-  onMount(() => {
-    status = "Preparing scan...";
+
+  async function init() {
+    closed = 0;
     Quagga.init(
       {
         inputStream: {
           name: "Live",
           type: "LiveStream",
-          target: document.querySelector("#scannerPreview"), // Or '#yourElement' (optional)
+          target: document.querySelector("#scannerPreview"),
           constraints: {
             width: 640,
             height: 480,
-            facingMode: "environment",
-            //, deviceId: 'TODO' // TODO reuse previously selected device id, keep device id in store
+            facingMode: "environment"
           },
-          singleChannel: false, // red only should suffice for EANs
+          singleChannel: false
         },
         decoder: {
           readers: ["ean_reader"],
-          multiple: false,
+          multiple: false
         },
         numOfWorkers: 4,
         frequency: 10,
-        debug: false,
+        debug: false
       },
-      function (err) {
+      function(err) {
         if (err) {
           console.log(err);
-          error = err;
           return;
         }
         _populateCameraDevices();
-        status = `Scanning for EAN...`;
         Quagga.start();
       }
     );
-    Quagga.onDetected((result) => {
+
+    Quagga.onDetected(result => {
       let foundEan = result.codeResult.code;
-      Quagga.stop();
-      status = "Found EAN " + foundEan;
+      // Quagga.stop();
+      console.log(`Found EAN: ${foundEan}`);
       setTimeout(() =>
         dispatch("ean", {
           ean: foundEan,
-          imageSrc: Quagga.canvas.dom.image.toDataURL(),
+          imageSrc: Quagga.canvas.dom.image.toDataURL()
         })
       );
     });
-    Quagga.onProcessed(function (result) {
-      if (!result) {
-        return;
-      }
-      let drawingCtx = Quagga.canvas.ctx.overlay,
-        drawingCanvas = Quagga.canvas.dom.overlay;
-      if (result.boxes) {
-        drawingCtx.clearRect(
-          0,
-          0,
-          parseInt(drawingCanvas.getAttribute("width")),
-          parseInt(drawingCanvas.getAttribute("height"))
-        );
-        result.boxes
-          .filter(function (box) {
-            return box !== result.box;
-          })
-          .forEach(function (box) {
-            Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
-              color: "green",
-              lineWidth: 2,
-            });
-          });
-      }
-      if (result.box) {
-        Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, {
-          color: "#00F",
-          lineWidth: 2,
-        });
-      }
-      if (result.codeResult && result.codeResult.code) {
-        Quagga.ImageDebug.drawPath(
-          result.line,
-          { x: "x", y: "y" },
-          drawingCtx,
-          { color: "red", lineWidth: 3 }
-        );
-      }
-    });
-    return () => {
-      Quagga.stop();
-    };
-  });
+  }
   function close() {
     Quagga.stop();
     closed = 1;
@@ -118,9 +80,6 @@
 <style>
   .wrap {
     text-align: center;
-  }
-  .error {
-    color: red;
   }
   #scannerPreview {
     position: relative;
@@ -133,40 +92,8 @@
     top: 0;
     left: 0;
   }
-  div[type="button"] {
-    border: 1px black solid;
-    background-color: pink;
-    -webkit-touch-callout: none;
-    -webkit-user-select: none;
-    -khtml-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-  }
-  div[type="button"]:active {
-    border: 2px black solid;
-    background-color: rgb(241, 167, 179);
-  }
 </style>
 
 <div class="wrap" style="display: {closed ? 'none' : 'block'};">
-  <p>{status}</p>
-
-  <p class="error">{error}</p>
-
-  <p>
-    {#if devices.length}
-      <select class="camera" bind:value={selectedDevice}>
-        {#each devices as device}
-          <option value={device}>{device.label}</option>
-        {/each}
-      </select>
-    {:else}Checking camera options...{/if}
-  </p>
-
   <div id="scannerPreview" />
-
-  <br />
-
-  <div type="button" on:click={close}>Stop scanning</div>
 </div>
