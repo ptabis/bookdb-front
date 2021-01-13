e8c72f8a88c8deec860fb588b44b5551e1eae02c
ptabis <p.tabis@protonmail.com>
2021-01-13 07:51:14 +0100

    add BookInfo component

---
+++ src/svelte/BookInfo.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { dataStore } from "../stores/dataStore.js";
+    let title;
+
+    dataStore.subscribe(value => {
+        title = value.data.title;
+    });
+</script>
+
+<style>
+</style>
+
+<div>
+    {#if title}<p>{title}</p>{/if}
+</div>
