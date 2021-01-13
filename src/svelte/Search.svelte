8e3d6279a89e18d0d4548f0af5a87f8c1a263c81
ptabis <p.tabis@protonmail.com>
2021-01-13 07:51:28 +0100

    add Search component

---
+++ src/svelte/Search.svelte
@@ -0,0 +1,22 @@
+<script>
+    import Quagga from "./Quagga.svelte";
+    import SearchButton from "./SearchButton.svelte";
+</script>
+
+<style>
+
+    .SearchButton {
+        position: fixed;
+        bottom: 10px;
+        right: 10px;
+    }
+</style>
+
+<div>
+    <div>
+      <Quagga />
+    </div>
+    <div class="SearchButton">
+      <SearchButton />
+    </div>
+</div>
